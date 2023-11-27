29b506150944fcf28dd6c31f5f1e557bab59eac1
Thomas <thomas.farineau@icloud.com>
2023-11-27 09:44:11 +0100

    add bash

--- externals.sh
+++ externals.sh
@@ -14,19 +14,19 @@ mvn clean package
 mkdir -p ../scenarios
 
 # Exécuter le DSL pour le scénario 'Very Simple Alarm'
-mvn exec:java -Dexec.args="src/main/resources/very_simple_alarm.arduinoml" > ../scenarios/very_simple_alarm_output.txt
+mvn exec:java -Dexec.args="src/main/resources/very_simple_alarm.arduinoml" | grep -v "[INFO]" > ../scenarios/very_simple_alarm_output.txt
 
 # Exécuter le DSL pour le scénario 'Dual Check Alarm'
-mvn exec:java -Dexec.args="src/main/resources/dual_check_alarm.arduinoml" > ../scenarios/dual_check_alarm_output.txt
+mvn exec:java -Dexec.args="src/main/resources/dual_check_alarm.arduinoml" | grep -v "[INFO]" > ../scenarios/dual_check_alarm_output.txt
 
 # Exécuter le DSL pour le scénario 'Multi State Alarm'
-mvn exec:java -Dexec.args="src/main/resources/multi_state_alarm.arduinoml" > ../scenarios/multi_state_alarm_output.txt
+mvn exec:java -Dexec.args="src/main/resources/multi_state_alarm.arduinoml" | grep -v "[INFO]" > ../scenarios/multi_state_alarm_output.txt
 
 # Exécuter le DSL pour le scénario 'State Based Alarm'
-mvn exec:java -Dexec.args="src/main/resources/state_based_alarm.arduinoml" > ../scenarios/state_based_alarm_output.txt
+mvn exec:java -Dexec.args="src/main/resources/state_based_alarm.arduinoml" | grep -v "[INFO]" > ../scenarios/state_based_alarm_output.txt
 
 mvn clean
 
-cd ../kernels/jvm
+cd ../../kernels/jvm
 
 mvn clean
