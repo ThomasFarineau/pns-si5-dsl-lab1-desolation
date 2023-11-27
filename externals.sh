fb76adb4169aa6309672647f5575c12ea00a146a
Thomas <thomas.farineau@icloud.com>
2023-11-27 10:01:53 +0100

    better externals.sh
    bash externals.sh
    -c for clear files
    -s "name" for exec only one scenario

--- externals.sh
+++ externals.sh
@@ -1,32 +1,68 @@
 #!/bin/bash
 
+# Définition des options d'argument
+clear_files=false
+specific_scenario=""
+
+# Traitement des options d'argument
+while getopts ":cs:" opt; do
+  case $opt in
+    c)
+      clear_files=true
+      ;;
+    s)
+      specific_scenario=$OPTARG
+      ;;
+    \?)
+      echo "Option invalide: -$OPTARG" >&2
+      exit 1
+      ;;
+    :)
+      echo "L'option -$OPTARG requiert un argument." >&2
+      exit 1
+      ;;
+  esac
+done
+
+# Fonction pour compiler un scénario
+compile_scenario() {
+    file=$1
+    filename=$(basename "$file" .arduinoml)
+
+    # Se déplacer vers le répertoire du DSL externe avec AntLR
+    cd ../../externals/antlr
+    # Nettoyer et construire le package
+    mvn clean package
+
+    mvn exec:java -Dexec.args="$file" | grep -v -e "[INFO]" -e "^Using input file:" > ../scenarios/"${filename}_temp.txt"
+    tail -n +3 ../scenarios/"${filename}_temp.txt" > ../scenarios/"${filename}.ino"
+    rm ../scenarios/"${filename}_temp.txt"
+    cd - > /dev/null
+}
+
+# Nettoyer les fichiers de sortie si l'option -c est présente
+if $clear_files ; then
+    rm -f ./externals/scenarios/*.ino
+    exit 0
+fi
+
 # Naviguer vers le répertoire du noyau JVM
 cd ./kernels/jvm
 # Nettoyer et installer les dépendances
 mvn clean install
 
-# Se déplacer vers le répertoire du DSL externe avec AntLR
-cd ../../externals/antlr
-# Nettoyer et construire le package
-mvn clean package
-
 # Créer le répertoire 'scenarios' s'il n'existe pas
-mkdir -p ../scenarios
+mkdir -p ../../externals/scenarios
 
-# Exécuter le DSL pour le scénario 'Very Simple Alarm'
-mvn exec:java -Dexec.args="src/main/resources/very_simple_alarm.arduinoml" | grep -v "[INFO]" > ../scenarios/very_simple_alarm_output.txt
-
-# Exécuter le DSL pour le scénario 'Dual Check Alarm'
-mvn exec:java -Dexec.args="src/main/resources/dual_check_alarm.arduinoml" | grep -v "[INFO]" > ../scenarios/dual_check_alarm_output.txt
-
-# Exécuter le DSL pour le scénario 'Multi State Alarm'
-mvn exec:java -Dexec.args="src/main/resources/multi_state_alarm.arduinoml" | grep -v "[INFO]" > ../scenarios/multi_state_alarm_output.txt
-
-# Exécuter le DSL pour le scénario 'State Based Alarm'
-mvn exec:java -Dexec.args="src/main/resources/state_based_alarm.arduinoml" | grep -v "[INFO]" > ../scenarios/state_based_alarm_output.txt
+# Compiler soit un scénario spécifique, soit tous les scénarios
+if [ -n "$specific_scenario" ]; then
+    compile_scenario "src/main/resources/$specific_scenario.arduinoml"
+else
+    for file in ../../externals/antlr/src/main/resources/*.arduinoml; do
+        compile_scenario "$file"
+    done
+fi
 
 mvn clean
-
 cd ../../kernels/jvm
-
 mvn clean
